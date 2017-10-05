08dec6bbf52fe1b27c97e60f9052960a8edb4b03
Thom-Ernst <thom@ernst.work>
2017-10-06 00:56:31 +0200

    Working in theory - good night

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from Downloader import Downloader
 from mp4tomp3 import m4tm3
 
 app = Flask(__name__)
-
+name = ""
 
 @app.route("/")
 def index():
@@ -16,13 +16,24 @@ def downloader():
     return render_template('downloader.html')
 
 
-@app.route("/downloader/process/", methods=['POST'])
+@app.route("/process", methods=['POST'])
 def process():
+    print("fetching...")
     url = request.form['inputUrl']
+    global name
     name = Downloader.getname(url)
     Downloader.getdata(url)
     res = '360p'
+    print("downloading...")
     Downloader.download(url, res)
+    print("converting...")
     m4tm3('./files/dump/', './files/fresh/')
+    return "Done!"
+
+
+@app.route("/downloader/processed")
+def processed():
+    global name
     print("returning file as mp3: {0}".format(name))
-    return send_from_directory('./files/fresh', '{0}.mp3'.format(name))
+    return send_from_directory('./files/fresh', '{0}.mp3'.format(name),
+                               as_attachment=True, attachment_filename=name + '.mp3')
